94af4fa30c1f0f93bd4d243795152152b536f21d
stephen <abordercollie@gmail.com>
2020-12-27 16:03:58 -0400

    misc fixes

--- install_artesky_srv.sh
+++ install_artesky_srv.sh
@@ -49,11 +49,11 @@ install.service.file() {
   echo 'Type=idle' >> ${SRVC_FILE}
 	echo 'PIDFile=/var/run/artesky_srv.pid' >> ${SRVC_FILE}
 	echo 'ExecStartPre=/bin/rm -f /var/run/artesky_srv.pid' >> ${SRVC_FILE}
-  echo 'ExecStart=/usr/local/bin/artesky_srv --ip ${1}' >> ${SRVC_FILE}
+  echo 'ExecStart='${install_dir}'/artesky_srv --ip '${1} >> ${SRVC_FILE}
   echo 'Restart=always' >> ${SRVC_FILE}
   echo 'RestartSec=10' >> ${SRVC_FILE}
   echo '' >> ${SRVC_FILE}
-  echo 'WorkingDirectory='${install_dir}'/noVNC/utils' >> ${SRVC_FILE}
+  echo 'WorkingDirectory='${install_dir}' ' >> ${SRVC_FILE}
   echo '' >> ${SRVC_FILE}
   echo 'ExecStop=/bin/kill -s QUIT $MAINPID' >> ${SRVC_FILE}
   echo 'KillSignal=SIGKILL' >> ${SRVC_FILE}
@@ -66,13 +66,13 @@ printf "\n[-] Get the BOOST Library\n\n"
 sudo apt install -y libboost-dev
 # https://github.com/Artesky/artesky-flat-box/blob/master/1.2/setup_artesky-flat_1.2.0.0_arm64_ubuntu.tar.xz
 printf "\n[-] Download the server code\n\n"
-mkdir -p ${install_dir}/setup_artesky
-cd ${install_dir}/setup_artesky
+mkdir -p ${install_dir}/artesky_bin
+cd ${install_dir}/artesky_bin
 git clone https://github.com/maudy2u/artesky_flat_box.git
 
 printf "\n[-] Download and Install the header files\n\n"
-mkdir -p ${install_dir}/setup_artesky/libartesky_SDK/1.0.0/include
-cd ${install_dir}/setup_artesky/libartesky_SDK/1.0.0/include
+mkdir -p ${install_dir}/artesky_bin/libartesky_SDK/1.0.0/include
+cd ${install_dir}/artesky_bin/libartesky_SDK/1.0.0/include
 curl -Ls -o "libartesky.h" "https://raw.githubusercontent.com/Artesky/artesky-projects-devel/master/libartesky_SDK/1.0.0/include/libartesky.h"
 curl -Ls -o "libartesky_defs.h" "https://raw.githubusercontent.com/Artesky/artesky-projects-devel/master/libartesky_SDK/1.0.0/include/libartesky_defs.h"
 curl -Ls -o "libartesky_errors.h" "https://raw.githubusercontent.com/Artesky/artesky-projects-devel/master/libartesky_SDK/1.0.0/include/libartesky_errors.h"
@@ -81,7 +81,7 @@ curl -Ls -o "libartlibarteskycesky.h" "https://raw.githubusercontent.com/Artesky
 curl -Ls -o "serial.h" "https://raw.githubusercontent.com/Artesky/artesky-projects-devel/master/libartesky_SDK/1.0.0/include/serial.h"
 
 printf "\n[-] Download and Install the binary libraries\n\n"
-cd ${install_dir}/setup_artesky
+cd ${install_dir}/artesky_bin
 curl -Ls -o "artesky_binaries.tar.xz" "https://github.com/Artesky/artesky-flat-box/raw/master/1.2/setup_artesky-flat_1.2.0.0_arm64_ubuntu.tar.xz"
 tar -xf artesky_binaries.tar.xz
 
@@ -93,7 +93,7 @@ cd ..
 
 printf "\n[-] Some clean up\n\n"
 rm ./artesky_binaries.tar.xz
-rm -rf ./bin ./lib ./libartesky_SDK ./plugin ./artesky_flat_box ./artesky_flat
+rm -rf ./bin ./lib ./libartesky_SDK ./plugin ./artesky_flat_box ./artesky-flat
 sudo apt remove -y libboost-dev
 
 printf "\n[-] Create User Service File and start\n\n"
